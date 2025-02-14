61e4d5fe8228525cfbf3a607ba6527b8b2510fa3
IZRINO <SX_916@163.com>
2025-02-14 16:04:43 +0800

    Add daisyUI, Tailwind CSS to project

--- README.md
+++ README.md
@@ -1,26 +1,11 @@
 # Astro + Vue Example
-这是一个Astro + Vue的初始模板
+这是一个Astro + Vue + daisyUI的初始模板
 
-This is an initial template for Astro + Vue
+This is an initial template for Astro + Vue + daisyUI
 
 使用此模板:
 
 Using this template:
 ```sh
-pnpm create astro@latest --template IZRINO/astro-vue-template
+pnpm create astro@latest --template IZRINO/astro-vue-template#daisyUI-vue
 ```
-
-# Branches
-- master:主分支 包含一个`Astro`官方的`Astro+Vue`示例，所有其他分支的更改基于此分支.
-    
-    master: The main branch includes an official `Astro+Vue` example by `Astro`, with all other branch changes based on this branch.
-
-- shadcn-vue: 一个基于master分支但是添加了 `TailwindCSS` 和 `shadcn-vue`的分支.
-    
-    shadcn-vue: A branch based on the master branch, incorporating `TailwindCSS` and `shadcn-vue`.
-- tauri-vue: 一个基于master分支但是添加了`tauri`的分支.
-  
-  tauri-vue: A branch based on the master branch with the addition of `tauri`.
-- tauri-shadcn-vue:一个基于`shadcn-vue`分支但是添加了`tauri`的分支.
-  
-  tauri-shadcn-vue: A branch based on `shadcn-vue` with added `Tauri` integration.
--- src/components/Counter.vue
+++ src/components/Counter.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import { ref } from 'vue';
+import { ref } from "vue";
 
 const count = ref(0);
 const add = () => count.value++;
@@ -7,26 +7,35 @@ const subtract = () => count.value--;
 </script>
 
 <template>
-	<div class="counter">
-		<button @click="subtract">-</button>
-		<pre>{{ count }}</pre>
-		<button @click="add">+</button>
-	</div>
-	<div class="counter-message">
-		<slot />
-	</div>
+  <div class="counter">
+    <button class="btn btn-success" @click="subtract">-</button>
+
+    <div class="card bg-base-100 w-96 shadow-sm">
+      <div class="card-body items-center text-center">
+        <div class="card-title text-5xl">
+          <slot />
+        </div>
+
+        <div
+          class="bg-neutral rounded-box text-neutral-content flex flex-col p-2 mt-10"
+        >
+          <span class="countdown font-mono text-5xl">
+            <span :style="'--value: ' + count"></span>
+          </span>
+        </div>
+      </div>
+    </div>
+
+    <button class="btn btn-success" @click="add">+</button>
+  </div>
 </template>
 
 <style>
 .counter {
-	display: grid;
-	font-size: 2em;
-	grid-template-columns: repeat(3, minmax(0, 1fr));
-	margin-top: 2em;
-	place-items: center;
-}
-
-.counter-message {
-	text-align: center;
+  display: grid;
+  font-size: 2em;
+  grid-template-columns: repeat(3, minmax(0, 1fr));
+  margin-top: 2em;
+  place-items: center;
 }
 </style>
